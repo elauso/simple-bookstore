adfb0760722829958984e31509c4f55ac3a80a76
Evandro <elauso@gmail.com>
2021-01-17 20:12:02 -0300

    Ajustando pacotes

--- src/main/kotlin/net/elau/example/simplebookstore/infra/config/DependencyConfig.kt
+++ src/main/kotlin/net/elau/example/simplebookstore/infra/config/DependencyConfig.kt
@@ -1,13 +1,18 @@
-package net.elau.example.simplebookstore.infra.`interface`.config
+package net.elau.example.simplebookstore.infra.config
 
+import net.elau.example.simplebookstore.domain.entity.student.StudentRepository
+import net.elau.example.simplebookstore.domain.usecase.student.register.RegisterStudentUseCase
 import net.elau.example.simplebookstore.infra.`interface`.db.student.StudentRepositoryJdbcTemplate
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.jdbc.core.JdbcTemplate
 
 @Configuration
-class EntityDependencyConfig {
+class DependencyConfig {
 
     @Bean
     fun studentRepository(jdbcTemplate: JdbcTemplate) = StudentRepositoryJdbcTemplate(jdbcTemplate)
+
+    @Bean
+    fun registerStudentUseCase(studentRepository: StudentRepository) = RegisterStudentUseCase(studentRepository)
 }
